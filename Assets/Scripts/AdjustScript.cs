29b002c0d88b757cf9705891a3c04fe8ed09f7cb
Charlie <charlieryancurry@gmail.com>
2024-09-24 13:46:37 -0300

    add saving to a file

--- Assets/Scripts/AdjustScript.cs
+++ Assets/Scripts/AdjustScript.cs
@@ -6,29 +6,62 @@ public class AdjustScript : MonoBehaviour
 {
     private void OnGUI()
     {
-        if (GUI.Button(new Rect(10, 10, 100, 30), "Adjust Health"))
+        if (GUI.Button(new Rect(200, 10, 150, 30), "Health Up"))
         {
             GameManager.manager.health += 10;
         }
-        if (GUI.Button(new Rect(10, 40, 100, 30), "Adjust Experience"))
+        if (GUI.Button(new Rect(200, 40, 150, 30), "Health Down"))
+        {
+            GameManager.manager.health -= 10;
+        }
+        if (GUI.Button(new Rect(200, 70, 150, 30), "Experience Up"))
         {
             GameManager.manager.experience += 10;
         }
-        if (GUI.Button(new Rect(10, 70, 100, 30), "Adjust Level"))
+        if (GUI.Button(new Rect(200, 100, 150, 30), "Experience Down"))
+        {
+            GameManager.manager.experience -= 10;
+        }
+        if (GUI.Button(new Rect(200, 130, 150, 30), "Level Up"))
         {
             GameManager.manager.level += 1;
         }
-        if (GUI.Button(new Rect(10, 100, 100, 30), "Adjust Gold"))
+        if (GUI.Button(new Rect(200, 160, 150, 30), "Level Down"))
+        {
+            GameManager.manager.level -= 1;
+        }
+        if (GUI.Button(new Rect(200, 190, 150, 30), "Gold Up"))
         {
             GameManager.manager.gold += 10;
         }
-        if (GUI.Button(new Rect(10, 130, 100, 30), "Adjust Score"))
+        if (GUI.Button(new Rect(200, 220, 150, 30), "Gold Down"))
+        {
+            GameManager.manager.gold -= 10;
+        }
+        if (GUI.Button(new Rect(200, 250, 150, 30), "Score Up"))
         {
             GameManager.manager.score += 10;
         }
-        if (GUI.Button(new Rect(10, 160, 100, 30), "Adjust Lives"))
+        if (GUI.Button(new Rect(200, 280, 150, 30), "Score Down"))
+        {
+            GameManager.manager.score -= 10;
+        }
+        if (GUI.Button(new Rect(200, 310, 150, 30), "Lives Up"))
         {
             GameManager.manager.lives += 1;
+        }
+        if (GUI.Button(new Rect(200, 340, 150, 30), "Lives Down"))
+        {
+            GameManager.manager.lives -= 1;
+        }
+
+        if (GUI.Button(new Rect(550, 400, 150, 30), "Save"))
+        {
+            GameManager.manager.Save();
+        }
+        if (GUI.Button(new Rect(550, 430, 150, 30), "Load"))
+        {
+            GameManager.manager.Load();
         }
     }
 }
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,6 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using System;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
 
 public class GameManager : MonoBehaviour
 {
@@ -50,5 +53,51 @@ public class GameManager : MonoBehaviour
         GUI.Label(new Rect(10, 130, 100, 30), "Score: " + score);
         GUI.Label(new Rect(10, 160, 100, 30), "Lives: " + lives);
         GUI.Label(new Rect(10, 190, 150, 30), "Game Manager Count: " + gameManagerCount);
+    }
+
+    public void Save()
+    {
+        BinaryFormatter bf = new BinaryFormatter();
+        FileStream file = File.Create(Application.persistentDataPath + "/playerInfo.dat");
+
+        PlayerData data = new PlayerData();
+        data.health = health;
+        data.experience = experience;
+        data.level = level;
+        data.gold = gold;
+        data.score = score;
+        data.lives = lives;
+
+        bf.Serialize(file, data);
+        file.Close();
+    }
+
+    public void Load()
+    {
+        if (File.Exists(Application.persistentDataPath + "/playerInfo.dat"))
+        {
+            BinaryFormatter bf = new BinaryFormatter();
+            FileStream file = File.Open(Application.persistentDataPath + "/playerInfo.dat", FileMode.Open);
+            PlayerData data = (PlayerData)bf.Deserialize(file);
+            file.Close();
+
+            health = data.health;
+            experience = data.experience;
+            level = data.level;
+            gold = data.gold;
+            score = data.score;
+            lives = data.lives;
+        }
+    }
+
+    [Serializable]
+    class PlayerData
+    {
+        public float health;
+        public float experience;
+        public int level;
+        public int gold;
+        public int score;
+        public int lives;
     }
 }
